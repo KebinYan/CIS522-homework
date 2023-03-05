79deb1ef93dc1500b6b960374311d74b47fba499
KebinYan <YANKEBIN99@GMAIL.COM>
2023-03-05 13:23:00 -0500

    add docstring

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -2,6 +2,9 @@ import torch
 
 
 class Model(torch.nn.Module):
+    '''
+    build a neural network
+    '''
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
         self.conv1 = torch.nn.Conv2d(3, 16, 3, padding=1)
@@ -11,6 +14,9 @@ class Model(torch.nn.Module):
         self.flatten = torch.nn.Flatten(start_dim=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+        construct model
+        '''
         x = torch.nn.functional.relu(self.conv1(x))
         x = self.pool(x)
         x = self.flatten(x)
