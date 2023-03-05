b850bc20d82246af26f06ab382efea2d434c55e4
KebinYan <YANKEBIN99@GMAIL.COM>
2023-03-05 14:51:56 -0500

    increase number of filters

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,8 +6,8 @@ from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 64
-    num_epochs = 5
+    batch_size = 128
+    num_epochs = 10
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
