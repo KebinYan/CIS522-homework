2a88cb128619db395474c479a44f6e88af8fc67f
KebinYan <YANKEBIN99@GMAIL.COM>
2023-03-08 15:57:48 -0500

    no maxpool

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -8,9 +8,8 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.conv1 = torch.nn.Conv2d(3, 16, 3, padding=1)
-        self.pool = torch.nn.MaxPool2d(2, 2)
-        self.fc1 = torch.nn.Linear(16 * 16 * 16, 10)
+        self.conv1 = torch.nn.Conv2d(3, 32, 3, padding=1)
+        self.fc1 = torch.nn.Linear(32 * 32 * 32, 10)
         self.flatten = torch.nn.Flatten(start_dim=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -18,7 +17,6 @@ class Model(torch.nn.Module):
         construct model
         """
         x = torch.nn.functional.relu(self.conv1(x))
-        x = self.pool(x)
         x = self.flatten(x)
         x = self.fc1(x)
         return x
