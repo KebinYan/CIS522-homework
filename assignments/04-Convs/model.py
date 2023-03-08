c7eca998bd7714ec6afad27fa38f4c66339e851b
KebinYan <YANKEBIN99@GMAIL.COM>
2023-03-08 18:00:37 -0500

    alter layer order

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,16 +10,17 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         self.conv1 = torch.nn.Conv2d(3, 16, 3, padding=1)
         self.bn1 = torch.nn.BatchNorm2d(16)
-        self.pool = torch.nn.MaxPool2d(4, 4)
-        self.fc1 = torch.nn.Linear(16 * 8 * 8, 10)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.fc1 = torch.nn.Linear(16 * 16 * 16, 10)
         self.flatten = torch.nn.Flatten(start_dim=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         construct model
         """
-        x = torch.nn.functional.relu(self.conv1(x))
+        x = self.conv1(x)
         x = self.bn1(x)
+        x = torch.nn.functional.relu(x)
         x = self.pool(x)
         x = self.flatten(x)
         x = self.fc1(x)
