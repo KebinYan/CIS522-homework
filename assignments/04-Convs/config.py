8b018245c7ee45dd9a709a5556d17f96faf40cb7
KebinYan <YANKEBIN99@GMAIL.COM>
2023-03-14 21:57:27 -0400

    try

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,11 +6,11 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 64
+    batch_size = 256
     num_epochs = 10
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-2)
 
     transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
