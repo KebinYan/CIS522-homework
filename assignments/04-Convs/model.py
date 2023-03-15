7fff092f8cd212656b7ed477a21949d886ebfd17
KebinYan <YANKEBIN99@GMAIL.COM>
2023-03-14 20:49:24 -0400

    try

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,8 +10,8 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         self.conv1 = torch.nn.Conv2d(3, 64, 3, padding=1)
         self.bn1 = torch.nn.BatchNorm2d(64)
-        self.pool = torch.nn.MaxPool2d(4, 4)
-        self.fc1 = torch.nn.Linear(64 * 8 * 8, 10)
+        self.pool = torch.nn.MaxPool2d(5, 5)
+        self.fc1 = torch.nn.Linear(64 * 6 * 6, 10)
         self.flatten = torch.nn.Flatten(start_dim=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
